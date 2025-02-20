cddd5f426d77956f6877f074e00f0bbaf2048b63
BG6TNB <BG6TNB@foxmail.com>
2025-02-20 18:08:25 +0800

    change vite

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/OSM-Offline-Map-Vue-Demo/',
   plugins: [
     vue(),
     vueDevTools(),
